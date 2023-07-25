ace3867827350bd33ca0193603f6833c0bcef375
seonyeongyoon <ssunnyy0204@gmail.com>
2023-07-25 09:19:13 +0900

    .

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -1,23 +1,21 @@
 import { getNode } from "../dom/getNode.js";
 
-const cache = {};
+export const memo = (() => {
+  const cache = {};
 
-export const memo = (key, callback) => {
-  if (!callback) return cache[key]; //callback이 없을 때는 해당 key를 가지고 있는 value를 리턴해라.
+  return (key, callback) => {
+    if (!callback) return cache[key];
+    if (cache[key]) {
+      console.warn(`${key}는 이미 캐시된 값이 존재합니다.`);
+      return cache[key];
+    }
+    cache[key] = callback();
+  };
+})();
 
-  if (cache[key]) {
-    //이미 key값이 존재할 때 경고메세지를 띄워줌.
-    console.warn(`${key}는 이미 캐시된 값이 존재합니다.`);
-    return cache[key]; //기존에 지정된 key값을 리턴해줌.
-  }
+memo("cube", () => getNode("#cube"));
+memo("cube", () => 123);
 
-  cache[key] = callback();
+console.log(memo("cube"));
 
-  console.log(cache);
-};
-
-memo("cube", () => getNode("#cube")); //setter
-
-console.log(memo("cube")); //div#cube. getter
-
-//객체를 만들어서 key:value 쌍으로 저장
+// 객체를 만들어서 key:value 쌍으로 저장
--- client/main.js
+++ client/main.js
@@ -37,7 +37,8 @@ const [startButton, recordButton, resetButton] = getNodes(
   ".buttonGroup > button"
 );
 const recordListWrapper = getNode(".recordListWrapper");
-const tbody = getNode(".recordList tbody");
+//const tbody = getNode(".recordList tbody");
+memo("@tbody", () => getNode(".recordList tbody")); //setter
 
 // 진짜 진짜 쉬운 과제
 
@@ -65,9 +66,9 @@ function createItem(value) {
 
 function renderRecordItem() {
   // 큐브의 data-dice 값 가져오기
-  const diceValue = +attr("#cube", "data-dice");
+  const diceValue = +attr(memo("cube"), "data-dice");
 
-  insertLast(tbody, createItem(diceValue));
+  insertLast(memo("@tbody"), createItem(diceValue));
 
   endScroll(recordListWrapper);
 }
@@ -111,7 +112,7 @@ function handleReset() {
   recordButton.disabled = true;
   resetButton.disabled = true;
 
-  clearContents(tbody);
+  clearContents(memo("@tbody"));
 
   count = 0;
   total = 0;
